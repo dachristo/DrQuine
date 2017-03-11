f18663e651af7f8609aa5a4d077047d0d2a77d60
xeiis <seis02@hotmail.fr>
2017-03-11 21:02:48 +0100

    added sully

--- sully.c
+++ sully.c
@@ -1,29 +1,20 @@
 #include <stdio.h>
-#include <string.h>
-
-int		ft_isdigit(int c)
+#include <fcntl.h>
+int main()
 {
-	if (c >= 48 && c <= 57)
-		return (1);
-	return (0);
-}
-
-int main(int argv, char **argc)
-{
-	if (ft_isdigit(argc[0][strlen(argc[0]) - 3]))
+	int i = 5;
+	char fn[10] = "Sully_x.c\0";
+	char cmd[42] = "clang ./Sully_x.c -o Sully_x; ./Sully_x\0";
+	
+	if (i > 0)
 	{
-		int i = argc[0][strlen(argc[0]) - 3];
-		char str[9] = "Sully";
-		str[5] = i;
-		str[6] = '.';
-		str[7] = 'c';
-		str[8] = '\0';
-		printf("%s\n", str);
-		//int fd = open(, O_WRONLY | O_CREAT);
-		//dprintf(
-	}
-	else
-	{
-		printf("c'est fini\n");
-	}
+		fn[6] = i + 47;
+		cmd[14] = i + 47;
+		cmd[27] = i + 47;
+		cmd[38] = i + 47;
+		int fd = open(fn, O_WRONLY | O_CREAT);
+		char *s = "#include<stdio.h>\n\nint main()\n{\t\t int i = %c;\n\tchar fn[10] = %c%s%c;\n\tchar cmd = %c%s%c;\n\n\tif (i > 0)\n\t{\n\t\tfn[6] = i + 47;\n\t\tint fd = open(fn);char *s =%c%s%c\n\t\tdprintf(fd,s,i+47,34,fn,34)\n\t\tsystem(cmd)\n\t}\n}\n";
+		dprintf(fd,s,i+47,34,fn,34,34,cmd,34,34,s,34);
+		system(cmd);
+	}	
 }
