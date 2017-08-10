3a363c35571dc3dd61e479858c97c53b6e9349cb
xeiis <seis02@hotmail.fr>
2017-08-10 15:58:19 +0200

    reworked sully js to replace i and start at 5

--- sully.js
+++ sully.js
@@ -1,22 +1,32 @@
 (function f() {
 	var fs = require('fs');
 	var new_sully = function (file, contenue) {
-		file.replace(i + 1, 'x');
 		fs.writeFile(file, contenue, function(err) {
 			if (err)
 				console.log(err);
 		});
-		console.log(file + " a été écrit");
 	}
 	var i = 5;
 	var file = "Sully_x.js";
 	var exec = require('child_process').exec;
-	var cmd = 'node ./Sully_x.js';
-	i--;
-	var file = file.replace('x', i);
-	new_sully(file, '(' + f.toString().replace(m, n).replace(l, m).replace(k, l).replace(j, k).replace(i, j) + ')()');
+	var cmd = 'node Sully_x.js';
+	var path = require('path');
+	var scriptName = path.basename(__filename);	
+	if (scriptName == "sully.js")
+	{	
+		var file = file.replace('x', i);
+		new_sully(file, '(' + f.toString() + ')()');
+	}
+	else if (i > 0)
+	{
+		i--;
+		var file = file.replace('x', i);
+		new_sully(file, '(' + f.toString().replace(/i = 1/g, 'i = 0').replace(/i = 2/g, 'i = 1').replace(/i = 3/g, 'i = 2').replace(/i = 4/g, 'i = 3').replace(/i = 5/g, 'i = 4') + ')()');
+	}
 	var cmd = cmd.replace('x', i);
-	exec(cmd, function(error, stdout, stderr) {
-		console.log("fin : ", i);
-	});
+	if (i > 0)
+	{
+		exec(cmd, function(error, stdout, stderr) {
+		});
+	}
 })()
